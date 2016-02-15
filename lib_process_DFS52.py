RD: fix units for t2 in NEMO case

lib_process_DFS52.py
@@ -485,7 +485,7 @@ class DFS52_processing():
 	                'units':'degC','fileout':self.output_dir + 't2_' + self.dataset + '_' + str(self.year) + '_ROMS.nc'}
 		elif self.target_model == 'NEMO':
 	                my_dict = {'varname':'t2','time_dim':'time','time_var':'time','long name':'Air Temperature at 2m',\
-	                'units':'degC','fileout':self.output_dir + 't2_' + self.dataset + '_' + str(self.year) + '.nc'}
+	                'units':'K','fileout':self.output_dir + 't2_' + self.dataset + '_' + str(self.year) + '.nc'}
 		my_dict['description'] = 'DFS 5.2 (MEOM/LGGE) contact : raphael.dussin@gmail.com'
 		my_dict['spval']          = self.spval
 		my_dict['reftime']        = self.reftime
@@ -523,8 +523,13 @@ class DFS52_processing():
 		# run the computation
 		for kt in np.arange(0,self.nframes):
 			# read all the fields
-			t2_old[:,:]    = ioncdf.readnc_oneframe(fid_t2old,self.name_t2,kt)
-			t2_new[:,:]    = ioncdf.readnc_oneframe(fid_t2new,self.name_t2,kt)
+			# ROMS has t2 in degC, others in Kelvin - qsat_from_t2_and_msl expects Celcius
+			if self.target_model == 'ROMS':
+				t2_old[:,:]    = ioncdf.readnc_oneframe(fid_t2old,self.name_t2,kt)
+				t2_new[:,:]    = ioncdf.readnc_oneframe(fid_t2new,self.name_t2,kt)
+			else:
+				t2_old[:,:]    = ioncdf.readnc_oneframe(fid_t2old,self.name_t2,kt) - 273.15
+				t2_new[:,:]    = ioncdf.readnc_oneframe(fid_t2new,self.name_t2,kt) - 273.15
 			msl[:,:]       = ioncdf.readnc_oneframe(fid_msl,self.name_msl,kt)
 			q2_tmp[:,:]    = ioncdf.readnc_oneframe(fid_q2,self.name_q2,kt)
 			# compute humidity at saturation

lib_process_ERAinterim.py
@@ -311,7 +311,9 @@ class ERAinterim_processing():
                 lat = self._readnc(fid_t2,'lat')
 		# run the computation
 		for kt in np.arange(0,self.nframes):
-			t2_out[kt,:,:] = self._readnc_oneframe(fid_t2,'T2M',kt) - 273.15
+			t2_out[kt,:,:] = self._readnc_oneframe(fid_t2,'T2M',kt)
+			if self.target_model == 'ROMS':
+				t2_out[kt,:,:] = t2_out[kt,:,:] - 273.15
 			this_time = dt.datetime(self.year,1,1,0,0) + dt.timedelta(seconds=int(kt)*86400/self.nframes_per_day)
                         time[kt] = (this_time - self.reftime).days + (this_time - self.reftime).seconds / 86400.
                 # close file
@@ -323,7 +325,7 @@ class ERAinterim_processing():
 	                self._write_ncfile(lon,lat[::-1],time,t2_out[:,::-1,:],my_dict)
 		elif self.target_model == 'NEMO':
 	                my_dict = {'varname':'t2','time_dim':'time','time_var':'time','long name':'Air Temperature at 2m',\
-	                'units':'degC','fileout':self.output_dir + 't2_' + self.dataset + '_' + str(self.year) + '.nc'}
+	                'units':'K','fileout':self.output_dir + 't2_' + self.dataset + '_' + str(self.year) + '.nc'}
 	                self._write_ncfile(lon,lat,time,t2_out,my_dict)
 		t2_out = None
                 return None